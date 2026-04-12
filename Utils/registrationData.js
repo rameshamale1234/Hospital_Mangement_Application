f116c1636c039cd0c1cd924b7fef125be72f9f8e
Ramesh <rameshamale50@gmail.com>
2026-04-12 11:19:07 +0530

    Fixed ES module issue completely

--- Utils/registrationData.js
+++ Utils/registrationData.js
@@ -1,12 +1,11 @@
-
 import { faker } from '@faker-js/faker';
 
 export function generateUserData() {
     return {
-        firstName: faker.person.firstName(),   
-        lastName: faker.person.lastName(),     
-        email: faker.internet.email(),        
-        phone: faker.phone.number('##########'), 
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        phone: faker.phone.number('##########'),
         password: faker.internet.password(8),
     };
 }
