f6e99b8b371297281ca0b215c886b754e2c44916
Ramesh <rameshamale50@gmail.com>
2026-04-12 11:13:35 +0530

    Converted to ES module fully

--- tests/PatientLoginTest.spec.js
+++ tests/PatientLoginTest.spec.js
@@ -1,5 +1,4 @@
-const {test, expect} = require('@playwright/test');
-import PatientLoginPage  from '../Pages/PatientLoginPage.js';
+import { test, expect } from '@playwright/test'; import PatientLoginPage  from '../Pages/PatientLoginPage.js';
 import patientLoginData from '../Utils/patientLoginData.json';    
 
 test('Patient Login Test', async ({ page }) => {
--- tests/RegistrationTest.spec.js
+++ tests/RegistrationTest.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test'; 
 import RegistrationPage from '../Pages/RegistrationPage.js';
 import { generateUserData } from '../Utils/registrationData.js';
 
